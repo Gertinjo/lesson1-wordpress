41c9a6eed7146df6d1149e31ed3ad2b617bd5cf3
Gertinjo <gerticalaj2013@gmail.com>
2025-12-01 19:16:14 +0100

    Style.css and front

---
+++ front-page.php
@@ -0,0 +1,76 @@
+<?php
+get_header() ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Front-Page</title>
+</head>
+<body class="body">
+    <img src="<?php echo esc_url(get_theme_file_uri("t.PNG")) ?> " class="imazhi1">
+    <h1 class="first-header"> Model 3 </h1>
+
+    <main>
+    <section class="kerret">
+        <h2 class=first-header4> Car Models </h2>
+    <div class="modeli3">
+    <img src="<?php echo esc_url(get_theme_file_uri("t1.png")) ?>" class="imazhi2"> 
+    <h1 class="first-header3"> Model 3 </h1>
+</div>
+<div class="modeliX">
+    <img src="<?php echo esc_url(get_theme_file_uri("t3.png")) ?>" class="imazhi2"> 
+    <h1 class="first-header3"> Model X </h1>
+<div class="modeliS">
+    <img src="<?php echo esc_url(get_theme_file_uri("t4.png")) ?>" class="imazhi2"> 
+    <h1 class="first-header3"> Model S </h1>
+<div class="panell">
+    <h2 class="first-header5"> More Things We Offer </h2>
+    <img src="<?php echo esc_url(get_theme_file_uri("t5.png")) ?>" class="imazhi2"> 
+    <h1 class="first-header3"> </h1>
+</div>
+<div class="frigo">
+    <img src="<?php echo esc_url(get_theme_file_uri("t6.png")) ?>" class="imazhi2"> 
+    <h1 class="first-header3"> </h1>
+</div>
+</div>
+</main>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php get_footer() ?>
